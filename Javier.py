1385b1413d6e0c08093dd6d2799e860dd88bb638
Jumpyvonvagabond <36978560+Jumpyvonvagabond@users.noreply.github.com>
2020-05-27 22:56:31 -0700

    added UI and edit ram function

--- Javier.py
+++ Javier.py
@@ -1,41 +1,50 @@
 import time
 import os
 import zipfile
-maxRAM = None  ##This is in terms of GIGABYTES
-if maxRAM == None:
-    try:
-        maxRAM = int(str(os.path.basename(__file__))[6:-3])
+def setRAM(maxRAM = ''):
+    while True:
+        try:
+            int(maxRAM)
+        except:
+            maxRAM = input("Please enter the amount of ram you'd like to dedicate to your Minecraft Server:\n")
+        else:
+            break
+    os.rename(os.path.basename(__file__),(f"Javier {str(maxRAM)}.py"))
+    input('The program needs to be restarted for changes to take effect.\nPress enter to close.')
+    raise SystemExit
 
-    except ValueError:
-        maxRAM = 'p'
-        print("The File isn't named after the amount of gigs of ram you'd like")
-        while(not maxRAM.isdigit()):
-            maxRAM = input("please enter the amount of ram you'd like to dedicate to this.")
-        os.rename(os.path.basename(__file__),(f"Javier {str(maxRAM)}.py"))
-        input('press anything to close this - re-open.')
-        sys.exit()
-
-
-
-file = None  # enter the JAR file here, make sure to put .jar at the end of it.
-if file == None:
-    files = []
-    for item in os.listdir():
-        if item[-4:] == '.jar':
-            files.append(item)
-    if len(files) > 1:
-        for item in files:
-            manafestcheck = zipfile.ZipFile(item,'r')
-            test = manafestcheck.open('META-INF/MANIFEST.MF','r')
-            garbagecheck = test.readlines()
-            for line in garbagecheck:
-                if 'net.minecraft.server.MinecraftServer' in str(line):
-                    files.remove(item)
-    file = files[0]
-customs = 'nogui'  ### Make sure to seperate customs with a space.
+try:
+    maxRAM = int(str(os.path.basename(__file__))[6:-3])
+except ValueError:
+    setRAM()
+else:
+    while True:
+        choice = input("\nWhat would you like to do?\n1) Run the Server\n2) Change the amount of dedicated RAM\n\n")
+        if choice == '1':
+            files = []
+            for item in os.listdir():
+                if item[-4:] == '.jar':
+                    files.append(item)
+            if len(files) > 1:
+                for item in files:
+                    check = zipfile.ZipFile(item,'r')
+                    check = check.open('META-INF/MANIFEST.MF','r')
+                    check = check.readlines()
+                    for line in check:
+                        if 'net.minecraft.server.MinecraftServer' in str(line):
+                            files.remove(item)
+                    del check
+            break
+        elif choice == '2':
+            setRAM()
+            break
+        else:
+            print("Invalid input")
+    del choice
+files = files[0]
 while True:
-    os.system(f"java -Xmx{maxRAM}G -Xms256M -jar {file} {customs}")
-    print("\n\nit seems as though the server has crashed or was stopped forcibly.\n\n\n\nrestarting the server in 10 seconds...")
-    for i in range (10, 0, -1):
+    os.system(f"java -Xmx{maxRAM}G -Xms256M -jar {files} nogui")
+    print("\n\nit seems as though the server has crashed or was stopped forcibly.\n\n\n\nRestarting the server in 3 seconds...")
+    for i in range (3, 0, -1):
         print(str(i) + " seconds remaning\n\n\n")
         time.sleep(1)
