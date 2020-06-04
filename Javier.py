54040687f0597e756faa78c48b3c71fc732ba81a
Neeko <45524401+Neeko-iko@users.noreply.github.com>
2020-06-04 16:21:00 -0700

    1.6 release thanks a lot to jumpy
    added a ton of extra functionality.  all of which gave me headaches except the "nogui" toggle...

--- Javier.py
+++ Javier.py
@@ -3,12 +3,67 @@ import time
 import os
 import zipfile
 import json
-## The launch function
-toggle = True
+from tkinter import filedialog
+toggle = True 
 
 
 
-def safeTog(safeStr):
+def dump(dta):
+    with open(f'{os.path.dirname(__file__)}/Javier.json', 'w') as f:
+        json.dump(dta, f, indent=4)
+
+def addDir(gui):    ## code to create a new window that's basically a menu to add or delete directories.
+
+
+    def deleteDir(dirStr, dirwindow, gui):  ## function that saves the action to the json, 
+        print(dirStr)                        #then relaunches the window due to me not knowing how to update it in real time oops sorry.
+        data['dirs'].remove(dirStr)
+        dump(data)
+        dirwindow.destroy()
+        addDir(gui)
+        
+
+    def addNewDir(dirwindow, gui, dirStr):     # creates an empty dir for the user to update
+        directory = filedialog.askdirectory(master =dirwindow, initialdir=".", title= "Select a folder", mustexist=True)
+        data['dirs'].append(directory)
+        dump(data)
+        dirwindow.destroy()
+        addDir(gui)
+
+    
+
+    dirwindow = tkinter.Toplevel(gui, bg = "DARK GREY")   # this makes the new window
+    label = tkinter.Label(dirwindow, text="Add your directories below!", bg = "DARK GREY", fg="BLACK")  # creates the label saying "enter the dirs!"
+    label.pack()  # this is required for the label to show up, if its not packed it may as well not exist lmao
+
+    buttonContainer = tkinter.Frame(dirwindow, bg= '#40444B',height = 160, width = 120)  ## frame for the scrolling container.
+    buttonCanvas = tkinter.Canvas(buttonContainer, bg= '#40444B', height = 150, width = 123) ## canvas that goes into the frame because frames can't have scrollbars because tkinter is awful.
+    scroll = tkinter.Scrollbar(buttonContainer, orient='vertical', command = buttonCanvas.yview, bg= '#40444B') #the scroll bar itself
+    buttonFrame = tkinter.Frame(buttonCanvas, bg= '#40444B')  # the frame that the buttons go in because iirc they're wonky on canvas..
+    buttonFrame.bind("<Configure>",lambda e: buttonCanvas.configure(scrollregion=buttonCanvas.bbox("all"))) 
+    buttonCanvas.create_window((0,0), window=buttonFrame, anchor = 'nw')
+    buttonCanvas.configure(yscrollcommand=scroll.set)                    
+
+    for i in range(0, len(data['dirs'])):  ## FOR loop makes buttons according to how many there are in 
+        direntry = tkinter.Entry(buttonFrame, width =14, bg="WHITE", fg="BLACK")  ## an entry is a small textbox, they're cleaner than just big text blocks imo.
+        direntry.insert(0, data['dirs'][i])  ## this inserts the name of the DIR into the text box. allowing the user to edit it or something ig idk this entire thing barely works ngl.
+        direntry.grid(row = i+1, column =0)  ## grid tells where to place it.  run it to understand it.
+        delete = tkinter.Button(buttonFrame, width = 1, text= "X", bg = "RED", fg="WHITE", command = lambda i= direntry.get(): deleteDir(i, dirwindow, gui))  ## this is the X button that deletes the DIR from the list in the JSON
+        delete.grid(row = i+1, column = 1) 
+        place = i+2  ## im bad.
+    
+    addnewButton = tkinter.Button(buttonFrame, width = 14, text = "ADD NEW DIR", bg = "GREEN", fg = "WHITE", command = lambda: addNewDir(dirwindow, gui, direntry.get()))  ## button to run code to add empty dir for user to edit.
+    addnewButton.grid(row = place, column = 0)
+
+    buttonContainer.pack() # the scrolling frame is packed here isntead of earlier - it doesn't really matter where its packed as long as it is,
+    buttonCanvas.pack(side = 'left', fill = 'both')
+    scroll.pack(side = 'right',fill = 'y')
+
+
+
+
+
+def safeTog(safeStr):  # code to toggle the safemode on/off
     safe = safeStr.get()[10:]
     if safe == "OFF":
         safeStr.set("Safemode: ON")
@@ -16,8 +71,17 @@ def safeTog(safeStr):
         safeStr.set("Safemode: OFF")
     return safeStr
 
+def guiTog(guiStr):  # code to toggle the nogui flag on/off
+    tog = guiStr.get()[5:]
+    if tog == "ON":
+        guiStr.set("GUI: OFF")
+    else:
+        guiStr.set("GUI: ON")
+    return guiStr
 
-def ThemeChange(theme, gui):
+
+
+def ThemeChange(theme, gui):  # code to update the theme.  still requires a restart as idk how to make javier update in real time.
     global data
     data["curTheme"] = theme
     jsonfile = open("./Javier.json", 'w')
@@ -25,11 +89,14 @@ def ThemeChange(theme, gui):
     jsonfile.close()
     print("These changes will apply next time Javier is launched.")
 
-def aboutWindow():
+def aboutWindow():  # this literally just opens the users browser to take them to the FAQ.
     import webbrowser
     webbrowser.open_new("https://github.com/Neeko-iko/JavierLauncher#FAQ")
     del webbrowser
-def conformWindow(gui):
+
+
+
+def conformWindow(gui):   # this deforms the window to show the settings page - im pretty sure its really inefficient but its also really cool so leave me be please :C
     global toggle
     if toggle:
         toggle = False
@@ -41,32 +108,31 @@ def conformWindow(gui):
         gui.title("Javier - MCSL")
         gui.maxsize(250, 363)
         gui.minsize(250, 363)
+def dataupdate(server):  # when adding launch flags Javier would complain about indexing, so i have it make a list for every server you click on.
+    data['servers'][server] = ['', '']
+    jsonfile = open("./Javier.json", 'w')
+    json.dump(data, jsonfile, indent = 4)
+    jsonfile.close()
 
-def confirmUpdate(server, RAM, selectStr, customs):
+def confirmUpdate(server, RAM, selectStr, customs, d):  # code to select a server, not launch it.
+    global dire
+    dire = d
 
-    def dataupdate():
-        data['servers'][server] = ['', '']
-        jsonfile = open("./Javier.json", 'w')
-        json.dump(data, jsonfile, indent = 4)
-        jsonfile.close()
-
-
-
-        #awful check to see if the key is wrong, or if the index is incorrect.
+    #awful check to see if the key is wrong, or if the index is incorrect.
     try:
         data['servers'][server][2]
     except KeyError:
-        dataupdate()
+        dataupdate(server)
     except IndexError:
         pass
 
 
 
-    selectStr.set(f"Start: {server}")
+    selectStr.set(f"Start: {server}") # this updates the start button to show the server name in the worst way possible.
     try:
         if data['servers'][server][0] != '':
             RAM.delete(0, "end")
-            RAM.insert(0,data['servers'][server][0])
+            RAM.insert(0,data['servers'][server][0])  # inserts the ram value found in the JSON
         else:
             print("No Ram found insert it using the box if you may.")
     except KeyError:
@@ -74,15 +140,24 @@ def confirmUpdate(server, RAM, selectStr, customs):
     try:
         if data['servers'][server][1] != '':
             customs.delete(0, "end")
-            customs.insert(0, data['servers'][server][1])
+            customs.insert(0, data['servers'][server][1]) # inserts the flags found in the JSON.
     except KeyError:
         pass
+    return dire
 
 
 
 
-def javierLaunch(selectStr, RAM, gui, safeStr, customs):
-    # Reformat the JSON to properly work with seerver stuff ig idk this is a quick fix
+
+def javierLaunch(selectStr, RAM, gui, safeStr, customs, guiStr, dire):
+    dire = data['dirs'][dire]
+
+    nogui = ''
+    if guiStr.get()[5:] == "ON":
+        nogui==""
+    else:
+        nogui=="nogui"
+    
 
     if selectStr.get() == "Start: None":
         print("please select a server")
@@ -130,13 +205,13 @@ def javierLaunch(selectStr, RAM, gui, safeStr, customs):
     ## Code to find the right JAR file to launch
     file = []
     safe = safeStr.get()[10:]
-    for item in os.listdir(f"./{server}"):
+    for item in os.listdir(f"{dire}/{server}"):
         if item[-4:] == '.jar':
             file.append(item)
     if len(file) > 1:
         for item in file:
             print(item)
-            check = zipfile.ZipFile(f"./{server}/{item}",'r')
+            check = zipfile.ZipFile(f"{dire}/{server}/{item}",'r')
             check = check.open('META-INF/MANIFEST.MF','r')
             check = check.readlines()
             for line in check:
@@ -146,77 +221,100 @@ def javierLaunch(selectStr, RAM, gui, safeStr, customs):
                         break
                     else:
                         file.remove(item)
-    if safe != "ON":
-        file = file[0]
-    #del safe, check
+    try:
+        if safe != "ON":
+            file = file[0]
+    except:
+        print("something went wrong. . .")
+        return
     ## Actual launch code.
     back = os.getcwd()
-    gui.destroy()
+    gui.destroy()   #DESTORYS JAVIER WITH FACTS AND LOGIC - because i don't wanna thread him yet.  i'll do that after 1.6 - it'll probably have to be a 2.0 update.
+    #del check, safe
+
     while True:
-        os.chdir(f"./{server}")
-        os.system(f"java -Xmx{maxRAM}G -Xms256M {customs} -jar {file} nogui")
+        os.chdir(f"{dire}/{server}")
+        os.system(f"java -Xmx{maxRAM}G -Xms256M {customs} -jar {file} {nogui}")
 
         os.chdir(back)
         print("\n\nit seems as though the server has crashed or was stopped forcibly.\n\n\n\nrestarting the server in 10 seconds...")
         for i in range (10, 0, -1):
             print(str(i) + " seconds remaning\n\n\n")
             time.sleep(1)
+
+
 def getdata():
     ## Searching for the JSON file to pull RAM values from
     try:
-        jsonfile = open("./Javier.json")
-        data = json.load(jsonfile)
-        jsonfile.close()
+        with open(f'{os.path.dirname(__file__)}/Javier.json', 'r') as f:
+            data = json.loads(f.read())
     except FileNotFoundError:
-        jsonfile = open("./Javier.json", "w")
-        jsonfile.write("""{"servers":{},"themes":{"Dark": ["#FFFFFF", "#36393F"],"HC": ["#FFFFFF", "#161719"],"Light": ["#161719", "#FFFFFF"]},"curTheme":"Dark"}""")
-        jsonfile.close()
+        data = {"dirs":["."],"servers":{},"themes":{"Dark": ["#FFFFFF", "#36393F"],"HC": ["#FFFFFF", "#161719"],"Light": ["#161719", "#FFFFFF"]},"curTheme":"Dark"}
+        dump(data)
         jsonfile = open("./Javier.json")
         data = json.load(jsonfile)
         jsonfile.close()
     return data
-def getServers():
-    ## Finding every subfolder in the folder its in. 
-    servers = os.listdir('.')
-    for item in servers:
-        if os.path.isfile(item):
-            servers.remove(item)
-    servers.remove(os.path.basename(__file__))
+
+
+def getServers(dires):
+    servers = []
+    serv = []
+    ## finds every folder in the directory given.  if "javier" is passed, it checks the folder javier is in.
+    for dire in dires:
+        if not os.path.isdir(dire):
+            data['dirs'].remove(dire)
+            servers = [["ERROR - RESTART"]]
+            jsonfile = open("./Javier.json", 'w')
+            json.dump(data, jsonfile, indent = 4)
+            jsonfile.close()
+            return servers
+
+        with os.scandir(dire) as lservers:
+            for item in lservers:
+                if os.path.isdir(os.path.abspath(item)):
+                    serv.append(item.name)
+            servers.append(serv)
+            serv = []
     return servers
+
+
 ## GUI code
 def runGUI():
 
+
     gui = tkinter.Tk()
     gui.title("Javier - MCSL")
+    gui.configure(bg = 'white')
+    gui.minsize(250, 363)
+    gui.maxsize(250, 363)
+    ## IK it seems redundant to make the min/max the same, but it makes Javier not resizable, saving me a lot of headache.  don't try to change this, it won't help you.
 
-    background = data["themes"][data["curTheme"]][1]
+
+    background = data["themes"][data["curTheme"]][1]  #grabs the BG and FG colors for the theme the user is currently using.
     foreground = data["themes"][data["curTheme"]][0]
+
+    ## Frames
 
 
     JavierFrame = tkinter.Frame(gui, height =360, width = 250, bg = '#40444B')
     JavierFrame.grid(row=0, column = 0)
 
-    #(gui, height = 364, width = 250, bg = '#40444B')
     settingsFrame = tkinter.Frame(gui,height =363, width = 250, bg = '#40444B')
     settingsFrame.grid(row=0, column = 1)
 
     ramFrame = tkinter.Frame(JavierFrame, height = 60, width = 250, bg = '#40444B')
     ramFrame.pack()
 
-    buttonContainer = tkinter.Frame(JavierFrame, bg= '#40444B',height = 60, width = 255)
-    buttonCanvas = tkinter.Canvas(buttonContainer, bg= '#40444B', height = 300, width = 229)
-    scroll = tkinter.Scrollbar(buttonContainer, orient='vertical', command = buttonCanvas.yview, bg= '#40444B')
-    buttonFrame = tkinter.Frame(buttonCanvas, bg= '#40444B')
-
-    buttonFrame.bind("<Configure>",lambda e: buttonCanvas.configure(scrollregion=buttonCanvas.bbox("all")))
-    buttonCanvas.create_window((0,0), window=buttonFrame, anchor = 'nw')
-
-    buttonCanvas.configure(yscrollcommand=scroll.set)
+    ## Buttons in settings and similar things.
 
     safeStr = tkinter.StringVar(value='Safemode: OFF')
     safeButton = tkinter.Button(settingsFrame, command = lambda: safeTog(safeStr), bd=2, height = 1, width = 12, textvariable = safeStr, bg = background, fg = foreground)
     safeButton.place(x=-1, y=297)
 
+    guiStr = tkinter.StringVar(value='GUI: OFF')
+    guiTogButton = tkinter.Button(settingsFrame, command = lambda: guiTog(guiStr), bd=2, height = 1, width = 8, textvariable = guiStr, bg = background, fg = foreground)
+    guiTogButton.place(x=92, y=297)
 
     customsFrame = tkinter.Frame(settingsFrame, bg=background)
     customsl = tkinter.Label(customsFrame, text = " ADVANCED: Launch Flags ", bg=background, fg=foreground)
@@ -226,35 +324,55 @@ def runGUI():
     customsFrame.place(x=0, y=323)
 
 
-    gui.minsize(250, 363)
-    gui.configure(bg = 'white')
-    gui.maxsize(250, 363)
+    ## server list code
 
-    for i in range(0, len(servers)):
-        launchButton = tkinter.Button(buttonFrame, text=servers[i], width = 32, height=1, command = lambda i=i : confirmUpdate(servers[i], RAM, selectStr, customs), bg=background, fg=foreground, bd=2)
-        launchButton.pack()
-
-    RAM = tkinter.Entry(ramFrame, bd =2,width = 3, bg=background, fg=foreground)
-    RAM.place(x=4, y=4)
-    label = tkinter.Label(ramFrame, text="< < < Enter RAM", height = 0, bg='#40444B', fg= "#FFFFFF")
-    label.place(x=27, y=4)
-
-    SettingsButton = tkinter.Button(ramFrame, bd = 1, width = 10, bg=background, fg= "#9999FF", text="Settings", command= lambda : conformWindow(gui))
-    SettingsButton.place(x=46, y=34)
-
-    aboutButton = tkinter.Button(ramFrame, bd=1, width = 4, bg=background, fg = "#FF9999", text = 'FAQ', command = aboutWindow)
-    aboutButton.place(x=4, y=34)
+    buttonContainer = tkinter.Frame(JavierFrame, bg= '#40444B',height = 60, width = 255)
+    buttonCanvas = tkinter.Canvas(buttonContainer, bg= '#40444B', height = 300, width = 229)
+    scroll = tkinter.Scrollbar(buttonContainer, orient='vertical', command = buttonCanvas.yview, bg= '#40444B')
+    buttonFrame = tkinter.Frame(buttonCanvas, bg= '#40444B')
+    buttonFrame.bind("<Configure>",lambda e: buttonCanvas.configure(scrollregion=buttonCanvas.bbox("all")))
+    buttonCanvas.create_window((0,0), window=buttonFrame, anchor = 'nw')
+    buttonCanvas.configure(yscrollcommand=scroll.set)
+    
+    count = 0
+    servers = getServers(data['dirs'])
+    for d in servers:
+        dlbl = tkinter.StringVar(value=data['dirs'][count])
+        listdirL = tkinter.Label(buttonFrame, textvariable = dlbl, bg = foreground, fg=background, width= 33) # label is made to categorize by dir instead of just dumping everything out at once.
+        if listdirL.cget('text') == '.':
+            dlbl.set("Javier")
+        listdirL.pack()
+        for i in range(0, len(servers[count])):
+            launchButton = tkinter.Button(buttonFrame, text=servers[count][i], width = 32, height=1, command = lambda i=i, d=d, count=count : confirmUpdate(servers[count][i], RAM, selectStr, customs, count), bg=background, fg=foreground, bd=2)
+            launchButton.pack()
+        count += 1
+    newdirButton = tkinter.Button(buttonFrame, text="Add a Directory. . .", width = 32, height = 1, command= lambda : addDir(gui), bg=foreground, fg=background, bd=2)  # persistant button at the bottom to add a DIR.
+    newdirButton.pack()
 
     buttonContainer.pack()
     buttonCanvas.pack(side = 'left', fill = 'both')
     scroll.pack(side = 'right',fill = 'y')
 
+    ## stuff in the ramFrame
+
+
+    RAM = tkinter.Entry(ramFrame, bd =2,width = 3, bg=background, fg=foreground)  # cute ram entry
+    RAM.place(x=4, y=4)
+    label = tkinter.Label(ramFrame, text="< < < Enter RAM", height = 0, bg='#40444B', fg= "#FFFFFF")
+    label.place(x=27, y=4)
+
+    SettingsButton = tkinter.Button(ramFrame, bd = 1, width = 10, bg=background, fg= "#9999FF", text="Settings", command= lambda : conformWindow(gui)) # the button that makes javier pop in and out his jacket.
+    SettingsButton.place(x=46, y=34)
+
+    aboutButton = tkinter.Button(ramFrame, bd=1, width = 4, bg=background, fg = "#FF9999", text = 'FAQ', command = aboutWindow)  # button to open the FAQ on javiers github.
+    aboutButton.place(x=4, y=34)
+
 
     selectStr = tkinter.StringVar(value="Start: None")
-    confirmBut = tkinter.Button(ramFrame, textvariable = selectStr, width = 17, height =3, command = lambda :  javierLaunch(selectStr, RAM, gui, safeStr, customs), bd=1, bg=background, fg=foreground)
+    confirmBut = tkinter.Button(ramFrame, textvariable = selectStr, width = 17, height =3, command = lambda :  javierLaunch(selectStr, RAM, gui, safeStr, customs, guiStr, dire), bd=1, bg=background, fg=foreground)
     confirmBut.place(x=127, y=4)
 
-    ### settings and stuff
+    ### Themes
 
 
     ThemeMenu = tkinter.Frame(settingsFrame, bg= '#30443B')
@@ -285,13 +403,9 @@ def runGUI():
     ThemeMenu.place(x=155, y=223)
 
 
-
-
-
-
     gui.mainloop()
 
-
+## actual script.
+dire = None
 data = getdata()
-servers = getServers()
 runGUI()
