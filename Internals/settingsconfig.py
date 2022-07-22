8d65475bfcf06bd139cf9b2876762f5f3f0e425a
Neeko_iko <niko.eeko@gmail.com>
2022-07-22 17:53:13 -0600

    changed from GTK to QT because im indecisive.

--- Internals/settingsconfig.py
+++ Internals/settingsconfig.py
@@ -1,12 +1,1 @@
-import gi
-gi.require_version("Gtk", "3.0")  # this is for GTK. 
-from gi.repository import Gtk as gtk
-
-
-class settingswindow(gtk.Window):
-    def __init__(self):
-        super().__init__(title = "settings")
-
-        self.settingbutton = gtk.Button(label = "open settings")
-        self.settingbutton.connect("clicked", self.dothing)
-        self.add(self.settingbutton)
+# TODO: add this LMAO LOL!!! GENIUS!!!
--- Javier.py
+++ Javier.py
@@ -1,38 +1,31 @@
 from http import server
 from Internals import serverfinder, settingsconfig
-import gi
-gi.require_version("Gtk", "3.0")  # this is for GTK. 
-from gi.repository import Gtk as gtk
+from PySide6 import QtCore, QtWidgets, QtGui
+import sys
+class MainScreen(QtWidgets.QWidget):
+    def __init__ (self):
+        super().__init__()
+        self.button = QtWidgets.QPushButton("Start Server!")
 
-class mainwindow(gtk.Window):
-    def __init__(self):
-        super().__init__(title = "JavierGTK")
-
-        #settingbutton = gtk.Button(label = "open settings") will work on this stuff L8R NERDS!!!!!!
-        #settingbutton.connect("clicked", self.opensettings)
-        #self.add(settingbutton)
-        listof = gtk.ListBox()
-        listof.set_selection_mode(gtk.SelectionMode.NONE)
-        #items = gtk.ListBoxRow()
-
-        serverlist = serverfinder.folders()
-        if serverlist == []:
-            emptylabel = gtk.Label(label = "Javier found no servers!")
-            listof.add(emptylabel)
-        else:
-            for servers in serverlist:
-                serverbutton = gtk.Button(label = servers)
-                listof.add(serverbutton)
-
-        #listof.add(items)
-        self.add(listof)
-
-    def opensettings(self, item):
-       win = settingsconfig.settingswindow()
-       win.show_all()
+        self.label = QtWidgets.QLabel("This doesn't work, and it won't for a long time.")
 
 
-window = mainwindow()
-window.show_all()
-window.connect("destroy", gtk.main_quit)
-gtk.main()
+        self.layout = QtWidgets.QVBoxLayout(self)
+        self.layout.addWidget(self.button)
+        self.layout.addWidget(self.label)
+
+        self.button.clicked.connect(self.findServers)
+
+
+    @QtCore.Slot()
+    def findServers(self):
+        self.label.setText(str(serverfinder.folders()))
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+
+    widget = MainScreen()
+    widget.resize(500, 696) # double the size of the original!!! that's right!! go big or go home!!!!!!!!!!!!!!!!!!
+    widget.show()
+    sys.exit(app.exec())
